8af32dafb9e99acdb10fe300777660fc74a37001
ksbrazier <kylebrazier@gmail.com>
2020-10-31 14:59:29 -0400

    unfix

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-const db = require("./models");
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}!`);
